d9875d500de515a18c77ec0189f7a102d638172f
Michelle Dick <michelle@Michelles-MacBook-Pro.local>
2015-11-23 14:03:48 -0800

    Map pins now render on map when country name is checked.  Still working on pin removal though.

--- public/js/countries-map.js
+++ public/js/countries-map.js
@@ -1,16 +1,4 @@
 
-//initialize the map
-var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 1
-});
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'chinbule.niakl76o',
-    accessToken: 'pk.eyJ1IjoiY2hpbmJ1bGUiLCJhIjoiMzY0Y2JlZDI1NGIwYzRhOTkxNGUzNGQzMDZjOTdlYWMifQ.ER66l7Pjj1PI1lVTkSrmJQ'
-}).addTo(map);
 
 // Make the map and country list the same height
 
--- public/js/grapevine-ng.js
+++ public/js/grapevine-ng.js
@@ -4,18 +4,43 @@
 
 
 		app.controller('SocketController', function($scope){
+
+			//initialize the map
+			var map = L.map('map', {
+			    center: [51.505, -0.09],
+			    zoom: 1
+			});
+
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			    maxZoom: 18,
+			    id: 'chinbule.niakl76o',
+			    accessToken: 'pk.eyJ1IjoiY2hpbmJ1bGUiLCJhIjoiMzY0Y2JlZDI1NGIwYzRhOTkxNGUzNGQzMDZjOTdlYWMifQ.ER66l7Pjj1PI1lVTkSrmJQ'
+			}).addTo(map);
+
+			var markerIcon = L.icon({
+			    iconUrl: 'img/marker-icon.png',
+			    iconAnchor:   [12, 40]
+			});
+
+			
+
 			var socket = io.connect('http://127.0.0.1:8081');
 			//$scope.socket=socket;
 			var that = this;
 
 			socket.on('ack', function (data) {
+				console.log(data);
 
 				$scope.isPinned = function(country) {
 					return country.checked ?  "pin" : "no-pin";
 				}
 				$scope.isChecked = function(country) {
+					updateMarker(country);
 					return country.checked ?  "fa fa-check-square-o" : "fa fa-square-o";
 				}
+
+
+				
 
 /*
 				socket.emit('search', 'gorilla attacks');
@@ -40,6 +65,34 @@
 					console.log("error: " + err);
 				});
 				$scope.$apply(function(){that.countries = data;});
+
+				//create a marker for each checked country
+				L.marker([-10.3333333, -53.2], {icon: markerIcon}).addTo(map);
+				
+				Object.keys(that.countries).forEach(function (key) {
+				    var country = that.countries[key];
+					updateMarker(country);
+				});
+
+				
+				
+				function updateMarker(country){
+					if(country.checked==true){
+						console.log('updateMarker country=',country);
+						country['marker']=L.marker([country.latitude, country.longitude], {icon: markerIcon})
+						map.addLayer(country['marker']);
+						console.log('marker!= ',country['marker'])
+					} else {
+						//remove marker
+						console.log('updateMarker country=',country);
+					    map.removeLayer(country['marker']);
+					    console.log('marker!= ',country['marker'])
+					}
+				}
+
+				console.log(Object.getOwnPropertyNames(that.countries));
+				
+				
 
 
 			});
