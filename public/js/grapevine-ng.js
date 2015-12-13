440a2336deca25f8055ba72f97412a3f1c8c1dcf
Michelle Dick <michelle@Michelles-MacBook-Pro.local>
2015-12-13 00:31:45 -0800

    UI changes, map tags and map marker functionalities implemented.

--- public/js/grapevine-ng.js
+++ public/js/grapevine-ng.js
@@ -5,21 +5,35 @@
 
 		app.controller('SocketController', function($scope){
 
-			//initialize the map
-			var map = L.map('map', {
-			    center: [51.505, -0.09],
-			    zoom: 1
-			});
 
-			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-			    maxZoom: 18,
+
+			// create a map in the "map" div, set the view to a given place and zoom
+			var map = L.map('map', {
+			    layers: [
+			    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+			    maxZoom: 5,
+			    minZoom: 1,
 			    id: 'chinbule.niakl76o',
 			    accessToken: 'pk.eyJ1IjoiY2hpbmJ1bGUiLCJhIjoiMzY0Y2JlZDI1NGIwYzRhOTkxNGUzNGQzMDZjOTdlYWMifQ.ER66l7Pjj1PI1lVTkSrmJQ'
-			}).addTo(map);
+			})],
+			    zoom: 1,
+			    center: [49.009952, 2.548635],
+			    //        maxBounds: [[-90.0,-180.0],[90.0,180.0]]
+			    maxBounds: [
+			        [-85.0, -180.0],
+			        [85.0, 180.0]
+			    ]
+
+			});
 
 			var markerIcon = L.icon({
-			    iconUrl: 'img/marker-icon.png',
-			    iconAnchor:   [12, 40]
+			    iconUrl: 'img/marker-icon-red2.png',
+			    iconAnchor:   [8, 8]
+			});
+
+			var selectedMarkerIcon = L.icon({
+			    iconUrl: 'img/marker-icon-red.png',
+			    iconAnchor:   [8, 8]
 			});
 
 			
@@ -31,16 +45,37 @@
 			socket.on('ack', function (data) {
 				console.log(data);
 
-				$scope.isPinned = function(country) {
-					return country.checked ?  "pin" : "no-pin";
-				}
-				$scope.isChecked = function(country) {
+				$scope.countryClick = function(country) {
+					console.log('country click! ', country.checked);
+					country.checked = !country.checked;
 					updateMarker(country);
+					return
+				}
+
+				$scope.isChecked = function(country) {
 					return country.checked ?  "fa fa-check-square-o" : "fa fa-square-o";
 				}
 
-
-				
+				//doesnt work yet...supposed to update country list to implement autocomplete of input
+				$scope.updateList = function(countryInput){
+					console.log("meep");
+					for(country in data){
+						if(countryInput==''){
+							console.log('blank input');
+							continue;
+						}
+						if(data[country]['name'].toLowerCase().indexOf(countryInput.toLowerCase()) !== -1){
+							console.log('country',data[country]['name'].toLowerCase());
+							console.log(countryInput);
+							data[country]['display']=true;
+							console.log(data[country]['display']);
+						}else{
+							console.log('country',data[country]['name'].toLowerCase());
+							data[country]['display']=false;
+							console.log(data[country]['display']);
+						}
+					}
+				}
 
 /*
 				socket.emit('search', 'gorilla attacks');
@@ -65,35 +100,40 @@
 					console.log("error: " + err);
 				});
 				$scope.$apply(function(){that.countries = data;});
+				
+				console.log('what', that.countries);
 
-				//create a marker for each checked country
-				L.marker([-10.3333333, -53.2], {icon: markerIcon}).addTo(map);
-				
-				Object.keys(that.countries).forEach(function (key) {
-				    var country = that.countries[key];
-					updateMarker(country);
-				});
+				//pin all of the countries on the map
+				for(country in that.countries){
+					that.countries[country]['marker'] = L.marker([that.countries[country].latitude, that.countries[country].longitude], {icon: markerIcon, title: that.countries[country]['name']});
+					var options = {
+						  offset:  new L.Point(2, 10),
+						  closeButton: false
+						}
+						that.countries[country]['popup']=new L.Popup();
+						that.countries[country]['popup'].setContent(that.countries[country]['name']);
+						that.countries[country]['marker'].bindPopup(that.countries[country]['popup'], options).openPopup();
+						map.addLayer(that.countries[country]['marker']);
+				}
 
-				
-				
+				//on click, update map with appropriate pin color for selected/unselected countries
 				function updateMarker(country){
 					if(country.checked==true){
-						console.log('updateMarker country=',country);
-						country['marker']=L.marker([country.latitude, country.longitude], {icon: markerIcon})
-						map.addLayer(country['marker']);
-						console.log('marker!= ',country['marker'])
+						country['marker'].options.zIndexOffset=1000;
+						country['marker'].options.title=country['name'];
+						
+						country['marker'].setIcon(selectedMarkerIcon);
 					} else {
 						//remove marker
-						console.log('updateMarker country=',country);
-					    map.removeLayer(country['marker']);
-					    console.log('marker!= ',country['marker'])
+						country['marker'].options.zIndexOffset=0;
+						country['marker'].setIcon(selectedMarkerIcon);
+						country['marker'].setIcon(markerIcon);
 					}
 				}
 
-				console.log(Object.getOwnPropertyNames(that.countries));
-				
 				
 
+				console.log(Object.getOwnPropertyNames(that.countries));
 
 			});
 	
