41f24485f08b2fd630d7e46817a97a5410f60066
snakecharmer1024 <snakecharmr1024@gmail.com>
2015-12-13 00:33:20 -0800

    Small logging addition

--- grapevine_app.js
+++ grapevine_app.js
@@ -9,8 +9,11 @@ var server = require('http').Server(app);
 //instantiating socket.io module with server as argument
 //any socket made with socket io library will be bound to the server
 var io = require('socket.io')(server);
+
+var port = 8081
 //listens on localhost:8081 for event emissions
-server.listen(8081);
+server.listen(port);
+console.log('listening on port ' + port);
 
 var grapevine = require('./grapevine');
 //define the init function
